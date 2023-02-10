7b5872edf77963f0ce06edb5efdb129c2973c753
izeriayounes <younesizeria@gmail.com>
2023-02-10 16:17:53 +0100

    finnalize the app

---
+++ game.js
@@ -0,0 +1,64 @@
+var buttonColors = ['red', 'blue', 'yellow', 'green'];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+function playSound(color){
+    audio = new Audio('sounds/'+color+'.mp3');
+    audio.play();
+}
+function animate(color){
+    $('#' + color). fadeOut() .fadeIn();
+}
+function animateWhenClicked(color){
+    $('#' + color).addClass('pressed');
+    setTimeout(() => $('#' + color).removeClass('pressed'), 100);
+}
+function nextSequence(){
+    randomNumber = Math.floor(Math.random()*4);
+    randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);    
+    animate(randomChosenColor);
+    playSound(randomChosenColor);
+    $('h1').text('Level '+level);
+    return randomChosenColor;
+}
+function gameOverEffects(){
+    $('body').addClass('game-over');
+    setTimeout(() => $('body').removeClass('game-over'), 100);
+    $('.container').toggleClass('hidden');
+    playSound('wrong');
+    $('#start-btn').toggleClass('hidden');
+    $('h1').text('Game Over, Press restart');
+    gamePattern = [];
+    i=0;
+    level = 0;
+}
+if (level == 0){
+    $('.container').toggleClass('hidden');
+}
+var i = 0;
+
+$('.btn').click(function(){
+    userChosenColor = $(this).attr('id');
+    if (gamePattern[i] != userChosenColor){
+        gameOverEffects();
+        return;
+    }
+    animateWhenClicked($(this).attr('id'))
+    playSound($(this).attr('id'))
+    if (i<level){
+        i++;
+    }else{
+        level ++;
+        setTimeout(nextSequence, 1000);
+        i=0;
+    }
+})
+$('#start-btn').click(function() {
+    $('.container').toggleClass('hidden');
+    nextSequence();
+    $('h1').text('Level 1');
+    $(this).toggleClass('hidden');
+})
+
+
